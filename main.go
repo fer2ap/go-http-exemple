0dff43ba92b38bc21eb543b837387b0ad4f4bd83
fer2ap <pinto.faa@gmail.com>
2024-05-13 22:35:06 -0300

    + Read env var for tcp address + Listen and serve

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	ADDR = "ADDR"
+)
+
 func main() {
+
+	addr, err := os.LookupEnv(ADDR)
+
+	if !err {
+		log.Fatal("Couldn't find target address")
+	}
+
+	http.ListenAndServe(addr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { return }))
+
+	fmt.Println("Listening to port: ", addr)
+
 	return
 }
